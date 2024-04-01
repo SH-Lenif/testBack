a8dc4ed3b2355af4536a0e1ee98c9dcb58572134
SH-Lenif <76147656+SH-Lenif@users.noreply.github.com>
2024-04-01 14:06:43 +0900

    add post

--- pages/api/postData.js
+++ pages/api/postData.js
@@ -1,9 +1,10 @@
 // pages/api/data.js
 
-// // Amplify
-// import { Amplify } from 'aws-amplify';
-// import amplifyconfig from '../../src/amplifyconfiguration.json';
-// Amplify.configure(amplifyconfig);
+// Amplify
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../../src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
+
 
 export default function handler(req, res) {
     console.log('API 라우트가 호출되었습니다.');
@@ -14,18 +15,12 @@ export default function handler(req, res) {
             "data": [
             { "id": 1, "name": "John Doe", "email": "john@example.com" },
             { "id": 2, "name": "Jane Doe", "email": "jane@example.com" }
-            ]
+            ],
+            "amplifyconfig":"123"
         };
+        
         res.status(200).json(json_response);
-    }
-    else if (req.method === 'POST') {
-        // POST 요청을 처리하는 로직
-        const json_response = {
-            "data": ["POST"]
-        };
-        res.status(200).json(json_response);
-    }
-    else {
+    } else {
         // 다른 HTTP 메소드의 요청을 처리하지 않음
         res.setHeader('Allow', ['GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
